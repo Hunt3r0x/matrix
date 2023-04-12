f57036d401d9dc2467c047ef1a75eafa0938d955
Ahmed M <106396603+Hunt3r0x@users.noreply.github.com>
2023-04-12 19:59:59 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let count = 0;
+
+document.getElementById('matrixbutton').addEventListener('click', function () {
+    count++;
+    if (count == 1) {
+        alert('hello friend');
+    } else if (count == 2) {
+        alert('mmmmmm, you are looking for something');
+    } else if (count == 3) {
+        alert('click this link :');
+    }
+});
