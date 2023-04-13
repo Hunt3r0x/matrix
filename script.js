76d7f7180694037545a73cda618811c559ffd39f
Ahmed M <106396603+Hunt3r0x@users.noreply.github.com>
2023-04-13 01:53:14 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 let count = 0;
 
 document.getElementById('matrixbutton').addEventListener('click', function () {
-count++;
-if (count == 1) {
-alert('hello friend');
-} else if (count == 2) {
-alert('hmmm, you are looking for something');
-} else if (count == 3) {
-alert('click this link:');
-} else if (count == 6) {
-let videoLink = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"; // Replace with your video link
-let popup = window.open(videoLink, "popup", "width=800,height=600");
-setTimeout(function() { popup.close(); }, 10000); // Popup will close after 10 seconds
-count = 0; // Reset the click count
-}
+    count++;
+    if (count == 1) {
+        alert('hello friend');
+    } else if (count == 2) {
+        alert('hmmm, you are looking for something');
+    } else if (count == 3) {
+        alert('click this link:');
+    } else if (count == 6) {
+        let videoLink = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+        let popup = window.open(videoLink, "popup", "width=800,height=600");
+        setTimeout(function () { popup.close(); }, 100000);
+        count = 0;
+    }
 });
