eea6d7ffca00b9bc8fd6666d50fbd423010cb41f
Ahmed M <106396603+Hunt3r0x@users.noreply.github.com>
2023-04-13 02:45:29 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let count = 0;
 document.getElementById('matrixbutton').addEventListener('click', function () {
     count++;
     if (count == 1) {
-        alert('hello friend');
+        alert('Hello Friend');
     } else if (count == 2) {
         alert('hmmm, you are looking for something');
     } else if (count == 3) {
